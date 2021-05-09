9b4e3386bfefdb4b600223d9d53e0afe46529a34
陈锦锦 <45852874+cjj12138@users.noreply.github.com>
2021-05-09 16:48:10 +0800

    Update buyShoes.vue

--- src/components/UserAction/buyShoes.vue
+++ src/components/UserAction/buyShoes.vue
@@ -38,52 +38,17 @@
           </el-carousel>
         </div>
         <div style="position: fixed;margin-top: 20px">
-          <div>
-            <el-form ref="form" :model="form" label-width="80px">
-              <el-form-item label="活动名称">
-                <el-input v-model="form.name"></el-input>
-              </el-form-item>
-              <el-form-item label="活动区域">
-                <el-select v-model="form.region" placeholder="请选择活动区域">
-                  <el-option label="区域一" value="shanghai"></el-option>
-                  <el-option label="区域二" value="beijing"></el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item label="活动时间">
-                <el-col :span="11">
-                  <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                </el-col>
-                <el-col class="line" :span="2">-</el-col>
-                <el-col :span="11">
-                  <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                </el-col>
-              </el-form-item>
-              <el-form-item label="即时配送">
-                <el-switch v-model="form.delivery"></el-switch>
-              </el-form-item>
-              <el-form-item label="活动性质">
-                <el-checkbox-group v-model="form.type">
-                  <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                  <el-checkbox label="地推活动" name="type"></el-checkbox>
-                  <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                  <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                </el-checkbox-group>
-              </el-form-item>
-              <el-form-item label="特殊资源">
-                <el-radio-group v-model="form.resource">
-                  <el-radio label="线上品牌商赞助"></el-radio>
-                  <el-radio label="线下场地免费"></el-radio>
-                </el-radio-group>
-              </el-form-item>
-              <el-form-item label="活动形式">
-                <el-input type="textarea" v-model="form.desc"></el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                <el-button>取消</el-button>
-              </el-form-item>
-            </el-form>
-          </div>
+          <el-form ref="form" :model="shoes" label-width="80px">
+            <el-form-item label="商品标题">
+              <el-input v-model="shoes.goodName" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="商品描述">
+              <el-input v-model="shoes.goodDes" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格">
+              <el-input v-model="shoes.goodPrice" disabled></el-input>
+            </el-form-item>
+          </el-form>
         </div>
       </el-main>
     </el-container>
@@ -101,17 +66,7 @@ export default {
       goodList: [],
       isShow: false,
       isShowList: true,
-      good: {},
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
+      good: {}
     };
   },
   mounted() {
@@ -141,6 +96,7 @@ export default {
           res.data[0].goodPic[i] = require("@/assets/storeMsg/" + res.data[0].goodPic[i])
         }
         this.good = res.data[0]
+        this.shoes =this.good
       })
       console.log(item)
     }
